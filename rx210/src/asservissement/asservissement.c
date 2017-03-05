efb1195c4bc1293f66488a8e10afdc6ddfd4dec9
lcp5y3 <ylebansais@yahoo.com>
2017-03-05 22:39:05 +0000

    correction bug lecture uart + ajout fonction paramétrage pid

--- rx210/src/asservissement/asservissement.c
+++ rx210/src/asservissement/asservissement.c
@@ -5,26 +5,28 @@
 */
 
 #include "asservissement.h"
-
+#include "RPBRX210.h"
 //#include "odometrie.h"
-//#include "math.h"
+#include "math.h"
 
  PID PID_distance = {2.4,0,0};	// initialisation du pid pour la distance
  PID PID_orient = {1.2,0,0};	//inititalisation du pid pour l'orientation
  CMD cmd = {0x8000,0,0,0};
-
+ char transmit_data=0;
 /***********************************************************************************************
  * function of communication
  *********************************************************************************************/
-void send_pilot_mg(short *pwm){*pwm=cmd.pwmG;}	//send the value of the pmw left cmd
-void send_pilot_md(short *pwm){*pwm=cmd.pwmD;}	// send the value of the pwm right cmd
+void send_pilot_mg(short *pwm)	//send the value of the pmw left cmd
+{
+	char adresse= 3;
+	send_sht(&adresse,&(cmd.pwmG));
+}
+void send_pilot_md(short *pwm)	// send the value of the pwm right cmd
+{
+	char adresse = 4;
+	send_sht(&adresse,&(cmd.pwmD));
+}
 
-void load_dist_pid(short *dist){cmd.distance=*dist;}//change the value of distance pilotage
-void load_ang_pid(short *angl){cmd.angle = *angl;}	//change the value of angle driver
-
-void load_pd(float *p){PID_distance.kp = *p;}		//change value of pid distance
-void load_id(float *i){PID_distance.ki = *i;}
-void load_dd(float *d){PID_distance.kd = *d;}
 
 void send_pd(float *p)
 {
@@ -42,13 +44,45 @@ void send_dd(float *p)
 	send_flt(&adresse,&PID_distance.kd);		//send value of pid distance
 }
 
+void send_pa(float *p)		//send value of pid orient
+{
+	char adr = 10;
+	send_flt(&adr,&PID_orient.kp);
+}
+void send_ia(float *i)
+{
+	char adr = 11;
+	send_flt(&adr,&PID_orient.ki);
+}
+void send_da(float *d)
+{
+	char adr = 12;
+	send_flt(&adr,&PID_orient.kd);
+}
+void send_dist()
+{
+	char adresse = 9;
+	send_sht(&adresse,(compteur_d+compteur_g)/2);
+}
+void send_angl()
+{
+	char adr = 10;
+	send_sht(&adr,(compteur_d-compteur_g));
+}
+//fonction of receiption
+
+void load_dist_pid(short *dist){cmd.distance += *dist;}//change the value of distance pilotage
+void load_ang_pid(short *angl){cmd.angle += *angl;}	//change the value of angle driver
+
+void load_pd(float *p){PID_distance.kp = *p;}		//change value of pid distance
+void load_id(float *i){PID_distance.ki = *i;}
+void load_dd(float *d){PID_distance.kd = *d;}
+
 void load_pa(float *p){PID_orient.kp = *p;}		//change value of pid orient
 void load_ia(float *i){PID_orient.ki = *i;}
 void load_da(float *d){PID_orient.kd = *d;}
 
-void send_pa(float *p){*p = PID_orient.kp;}		//send value of pid orient
-void send_ia(float *i){*i = PID_orient.ki;}
-void send_da(float *d){*d = PID_orient.kd;}
+void transmission_data(char *value){transmit_data = *value;}
 
 
 /**********************************************************************************************
@@ -108,10 +142,23 @@ void inverser_gauche(int pwm){
 
 
 void Excep_MTU0_TCIV0(void) {
-	LED0=~LED0;
 	flag_over_te = 0;
 	reset_timer_te;
 	int a = compteur_d;
 	int b = compteur_g;
-	asservissement(cmd.distance,cmd.angle,a,b);
+	if(transmit_data==1)
+	{
+		send_dist();//fonction de reglage du pid
+		send_angl();
+		asservissement(cmd.distance,cmd.angle,a,b);
+		if(abs((compteur_d+compteur_g)-cmd.distance)<50 && abs((compteur_g-compteur_d)-cmd.angle)<50)
+		{
+			transmit_data = 0;
+			send_end_transmi();
+		}
+	}
+	else
+	{	
+		asservissement((compteur_d+compteur_g)/2,(compteur_d-compteur_g),a,b);
+	}
 }
